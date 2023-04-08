707c95ed744e6384066fcb7a443a8f2053243939
nakshCoder <nakshusharm0@gmail.com>
2023-04-08 09:36:30 +0530

    added some changes

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// $(".nav-tabs-dropdown")
-//   .on("click", ".nav-link:not('.active')", function (event) {
-//     $(this).closest("ul").removeClass("open");
-//   })
-//   .on("click", ".nav-link.active", function (event) {
-//     $(this).closest("ul").toggleClass("open");
-//   });
-
 let navBar = document.querySelector(".navbar-icon-box");
 let navItems1 = document.querySelector(".navbar-list-hidden");
 let navItems2 = document.querySelector(".navbar-list");
